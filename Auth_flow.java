1d77255a6a8373add88cf5ece4b6a905fbfefad8
thamizh29 <thamizharasan2510@gmail.com>
2025-06-22 23:30:07 +0530

    CI: auth flow was created signin signup

---
+++ Auth_flow.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Auth_flow{
+
+static class Auth{
+    String useremail, userpassword, email, password, method;
+    Scanner value = new Scanner (System.in);
+    
+    Auth(){
+        System.out.println("Enter the signin/sinup");
+        method = value.nextLine();
+        if (method.equalsIgnoreCase("signin")) {
+            signin();
+        }else{
+            signup();
+        }
+    }
+
+    void signup(){
+        System.out.println("Signup page");
+        System.out.println("Enter the email");
+        useremail = value.nextLine();
+        System.out.println("Enter the password");
+        userpassword = value.nextLine();
+        System.out.println("Successfully account was created");
+        signin();
+    }
+
+    void signin(){
+        System.out.println("Signin page");
+        System.out.println("Enter the email");
+        email = value.nextLine();
+        System.out.println("Enter the password");
+        password = value.nextLine();
+        if (email.equals(useremail) && password.equals(userpassword)) {
+            System.out.println("Signin success");
+        }else{
+            System.out.println("Invalid credential");
+        }
+    }
+}
+    public static void main(String[] args){
+        System.out.println("Welcome to auth_flow");
+        new Auth();
+    }
+}
